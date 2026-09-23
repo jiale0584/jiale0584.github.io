961aa6fbce02c71915770c9b9bc50a1425f83c0a
jiale0584 <jiale0584@gmail.com>
2026-09-23 15:33:23 +0000

    Deploying to gh-pages from @ jiale0584/jiale0584.github.io@b10210a35784e31f14c845daae4d11f798f8cef1 🚀

--- assets/al_math/js/mathjax-setup.js
+++ assets/al_math/js/mathjax-setup.js
@@ -1,26 +1,16 @@
 window.MathJax = {
   tex: {
     tags: "ams",
-    inlineMath: [
-      ["$", "$"],
-      ["\\(", "\\)"],
-    ],
+    inlineMath: [["$", "$"], ["\\(", "\\)"]],
+    macros: {},
   },
   options: {
     renderActions: {
-      addCss: [
-        200,
-        function (doc) {
-          const style = document.createElement("style");
-          style.innerHTML = `
-          .mjx-container {
-            color: inherit;
-          }
-        `;
-          document.head.appendChild(style);
-        },
-        "",
-      ],
+      addCss: [200, function () {
+        const style = document.createElement("style");
+        style.innerHTML = ".mjx-container { color: inherit; }";
+        document.head.appendChild(style);
+      }, ""],
     },
   },
 };
